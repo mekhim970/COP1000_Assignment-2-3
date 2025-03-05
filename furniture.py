8498794abb143999341c631c0bd79d143fff2be4
mekhim970 <mbmccray05@gmail.com>
2025-03-05 00:05:47 -0500

    check

--- furniture.py
+++ furniture.py
@@ -4,7 +4,7 @@ wholesale_price = 200.00
 profit = retail_price - wholesale_price
 sale_price = retail_price * 0.75
 sale_profit = sale_price - wholesale_price
-print("Item:", item)
+print("Item name:", item)
 print("Retail Price: $", format(retail_price, '.2f'), sep='')
 print("Wholesale Price: $", format(wholesale_price, '.2f'), sep='')
 print("Profit: $", format(profit, '.2f'), sep='')
